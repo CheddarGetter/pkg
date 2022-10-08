ddcd96e4995ed7b2fc82e9afbcae2da073e83272
John Doe <johndoe@example.com>
2022-10-08 15:39:09 -0700

    added comments

--- src/pkg_init.lua
+++ src/pkg_init.lua
@@ -1,13 +1,36 @@
-if not syn then return end -- synapse only
+-- synapse only!!! >:(
+if not syn then return end
 
-if not isfolder "pkg" then  -- create the pkg folder if it doesn't already exist
+-- create the pkg folder if it doesn't already exist
+if not isfolder "pkg" then
     makefolder "pkg"
 end
 
-if not isfile "pkg/controller.lua" then -- if the pkg controller is not installed
-    local response = syn.request {Url = ""}
-else -- if the pkg controller IS installed
+local function updateController()
+    -- send a request to get the latest controller version
+    local response = syn.request {Url = "https://raw.githubusercontent.com/CheddarGetter/pkg/main/src/controller.lua"}
 
-    -- automatically update the pkg controller if it's out of sync from the repo
-
+    if response.Success == true then
+        -- install the controller
+        writefile("pkg/controller.lua", response.Body)
+    else
+        -- woops something went wrong with the request, this darned internet i tell ya!
+        error(response.StatusCode .. ": " .. response.StatusMessage) -- tell me what went wrong... NOW!
+    end
 end
+
+-- if the pkg controller is not installed, install it
+if not isfile "pkg/controller.lua" then
+    updateController()
+end
+
+-- load that controllah! please no errors :^) (praying)
+local chunk, catch = loadfile "pkg/controller.lua"
+if catch then
+    error(catch) -- tell me why!!!!!!!
+end
+
+-- the user won't run the latest controller until this script is executed again
+-- personally i think this is better than yielding to get the latest version though
+-- change it if u so desire, this is the beauty of open source software!
+updateController()
