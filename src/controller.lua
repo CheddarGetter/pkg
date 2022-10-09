332662991bfb0de5278e1ee843da91884497afe9
John Doe <johndoe@example.com>
2022-10-08 21:25:50 -0700

    commit

--- src/controller.lua
+++ src/controller.lua
@@ -1,1 +1,75 @@
-print "temp"
+local b64Decode = syn_crypt_b64_decode
+local b64Encode = syn_crypt_b64_encode
+
+local moduleFolderUrl = "https://raw.githubusercontent.com/CheddarGetter/pkg/main/src/modules/%s.lua"
+
+local modules = {}
+local importQueue = {}
+local moduleLoaded = Instance.new("BindableEvent")
+
+local globalEnv = getgenv()
+
+local function httpGet(url, callback)
+    local response = syn.request {Url = url}
+
+    if response.Success == true then
+        callback(response)
+    else
+        warn(tostring(response.StatusCode) .. ": " .. tostring(response.StatusMessage))
+        return
+    end
+
+    return response
+end
+
+function globalEnv.import(url)
+    if importQueue[url] then
+        while moduleLoaded.Event:Wait() ~= url do end
+    end
+
+    if modules[url] then
+        return modules[url]
+    end
+
+    importQueue[url] = true
+
+    local b64Url = b64Encode(url)
+    local path = ("pkg/modules/%s.lua"):format(b64Url)
+
+    if not isfile(path) then
+        httpGet(url, function(response)
+            writefile(path, response.Body)
+        end)
+    end
+
+    local chunk, catch = loadfile(path)
+    if catch then
+        warn(catch)
+        return
+    end
+
+    local module = assert(chunk(), "Module does not return anything ")
+
+    importQueue[url] = nil
+    modules[url] = module
+
+    moduleLoaded:Fire(url)
+
+    return module
+end
+
+function globalEnv.getModule(moduleName)
+    return import(moduleFolderUrl:format(moduleName))
+end
+
+
+-- handle updating
+httpGet("https://raw.githubusercontent.com/CheddarGetter/pkg/main/src/controller.lua", function(response)
+    if readfile "pkg/controller.lua" == response.Body then return end
+
+    local choice = messagebox("would you like to update the pkg controller?", "pkg", 4)
+
+    if choice == 6 then
+        writefile("pkg/controller.lua", response.Body)
+    end
+end)
--- src/pkg_init.lua
+++ src/pkg_init.lua
@@ -1,39 +1,23 @@
--- synapse only!!! >:(
 if not syn then return end
 
--- create the pkg folder if it doesn't already exist
-if not isfolder "pkg" then
-    makefolder "pkg"
-end
+if not isfolder "pkg" then makefolder "pkg" end
+if not isfolder "pkg/modules" then makefolder "pkg/modules" end
 
-local function updateController()
-    -- send a request to get the latest controller version
+if not isfile "pkg/controller.lua" then
     local response = syn.request {Url = "https://raw.githubusercontent.com/CheddarGetter/pkg/main/src/controller.lua"}
 
     if response.Success == true then
-        -- install the controller
         writefile("pkg/controller.lua", response.Body)
     else
-        -- woops something went wrong with the request
-        error(response.StatusCode .. ": " .. response.StatusMessage) -- tell me what went wrong... NOW!
+        warn(tostring(response.StatusCode) .. ": " .. tostring(response.StatusMessage))
+        return
     end
 end
 
--- if the pkg controller is not installed, install it
-if not isfile "pkg/controller.lua" then
-    updateController()
-end
-
--- load that controllah! please no errors :^) (praying)
 local chunk, catch = loadfile "pkg/controller.lua"
 if catch then
-    error(catch) -- tell me why!!!!!!!
+    warn(catch)
     return
 end
 
 task.spawn(chunk)
-
--- the user won't run the latest controller until this script is executed again
--- personally i think this is better than yielding to get the latest version though
--- change it if u so desire, this is the beauty of open source software!
-updateController()
