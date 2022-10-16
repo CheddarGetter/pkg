73f8f3015bf943a88e99f7d0207878a4a5c1bf17
CheddarGetter <johndoe@example.com>
2022-10-15 21:03:03 -0700

    commit

--- src/modules/net.lua
+++ src/modules/net.lua
@@ -1,4 +1,4 @@
-local heartbeat = game:GetService("RunService")
+local heartbeat = game:GetService("RunService").Heartbeat
 local random = Random.new()
 
 local module = {}
@@ -13,24 +13,23 @@ function module:align(part0, part1)
     local alignPosition = Instance.new("AlignPosition")
     alignPosition.Attachment0 = at0
     alignPosition.Attachment1 = at1
-    alignPosition.ApplyAtCenterOfMass = true
-    alignPosition.ReactionForceEnabled = true
     alignPosition.RigidityEnabled = true
     alignPosition.Parent = part0
 
     local alignOrientation = Instance.new("AlignOrientation")
     alignOrientation.Attachment0 = at0
     alignOrientation.Attachment1 = at1
-    alignOrientation.ReactionTorqueEnabled = true
     alignOrientation.RigidityEnabled = true
     alignOrientation.Parent = part0
 end
 
 function module:keepOwnershipOf(part)
-    local thread = task.spawn(function()
-        while true do
-            part.AssemblyLinearVelocity = (Vector3.xAxis * random:NextNumber(60, 70))
-            part.AssemblyLinearVelocity = (Vector3.yAxis * random:NextNumber(60, 70))
+    local update = true
+
+    task.spawn(function()
+        while update do
+            part.AssemblyLinearVelocity = (Vector3.xAxis * random:NextNumber(27, 28))
+            part.AssemblyLinearVelocity = (Vector3.yAxis * random:NextNumber(27, 28))
 
             heartbeat:Wait()
         end
@@ -38,7 +37,7 @@ function module:keepOwnershipOf(part)
 
     local checkParent; checkParent = part:GetPropertyChangedSignal("Parent"):Connect(function()
         if part.Parent == nil then
-            task.cancel(thread)
+            update = nil
             checkParent:Disconnect()
         end
     end)
